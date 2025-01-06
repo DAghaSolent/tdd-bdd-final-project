2e6dadd0558580f94f83d849ef49be76d21838ff
DannyAgha <5aghad30@solent.ac.uk>
2025-01-06 04:28:15 -0500

    Fixed Linting Issues

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -172,7 +172,7 @@ class TestProductRoutes(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["name"], test_product.name)
-    
+
     def test_get_product_not_found(self):
         """It should not Get a Product thats not found"""
         response = self.client.get(f"{BASE_URL}/0")
@@ -252,7 +252,7 @@ class TestProductRoutes(TestCase):
         """It should Query Products by availability"""
         products = self._create_products(10)
         available_products = [product for product in products if product.available is True]
-        available_count = len(available_products)        
+        available_count = len(available_products)
         # test for available
         response = self.client.get(
             BASE_URL, query_string="available=true"
